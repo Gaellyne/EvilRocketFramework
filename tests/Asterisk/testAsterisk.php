5674dc967fad7fdeb62893ba9b8f1d43f2287664
Adel Shigabutdinov <drfaustxx@gmail.com>
2011-08-10 17:43:05 +0400

    Signed-off-by: Adel Shigabutdinov <drfaustxx@gmail.com>

--- tests/Asterisk/testAsterisk.php
+++ tests/Asterisk/testAsterisk.php
@@ -19,10 +19,12 @@ class testAsterisk extends PHPUnit_Framework_TestCase
     public function testConn()
     {
         $server = '192.168.1.188';
+        $port = "5038";
         $user = 'admin';
         $pass = 'amp111';
+        $phone = "89179273515";
 
-        $oSocket = fsockopen($server, 5038, $errnum, $errdesc) or die("Connection to host failed");
+        $oSocket = fsockopen($server, $port, $errnum, $errdesc) or die("Connection to host failed");
         if (!$oSocket)
         {
             echo $errdesc . '(' . $errnum . '.)'. PHP_EOL;
@@ -30,22 +32,31 @@ class testAsterisk extends PHPUnit_Framework_TestCase
         else
         {
             echo 'conn ok', PHP_EOL;
-            
+
             fputs($oSocket, "Action: login\r\n");
-            fputs($oSocket, "Events: off\r\n");
+            fputs($oSocket, "Events: on\r\n");
             fputs($oSocket, "Username: " . $user ."\r\n");
             fputs($oSocket, "Secret: " . $pass ."\r\n\r\n");
 
+          //  fputs($oSocket, "Action: ListCommands\r\n\r\n");
             
-            /*
-            fputs($oSocket, "Action: originate\r\n");
-            fputs($oSocket, "Channel: SIP/107\r\n");
+
+            fputs($oSocket, "Action: Originate\r\n");
+            fputs($oSocket, "Channel: LOCAL/".$phone."@from-internal\r\n");
+            fputs($oSocket, "Context: from-internal\r\n");
+            fputs($oSocket, "Exten: 1001\r\n");
             fputs($oSocket, "WaitTime: 120\r\n");
-            fputs($oSocket, "CallerId: open.kzn.ru\r\n");
-            fputs($oSocket, "Exten: 89179273515\r\n");
-            fputs($oSocket, "Context: sipnet\r\n");
+            fputs($oSocket, "Timeout: 30000\r\n");
+            fputs($oSocket, "CallerId: open.kzn.ru <89179666777>\r\n");
+            fputs($oSocket, "Async: yes\r\n");
             fputs($oSocket, "Priority: 1\r\n\r\n");
-            fputs($oSocket, "Action: Logoff\r\n\r\n"); */
+            
+
+            fputs($oSocket, "Action: Logoff\r\n\r\n");
+            while (!feof($oSocket))
+            {
+                echo fgets($oSocket, 128);
+            }
             fclose($oSocket);
         }
 
