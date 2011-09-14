48a40abc42dc892142ffe118e6ef77247a249fdf
Denis Makinder <denis.makinder.azarov@gmail.com>
2011-09-14 16:27:59 +0400

    add evil parser for resourse(Imdb.com,download.cnet.com, lastfm.com, gamespot.com)

---
+++ Parser/ImdbParser.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @desc class which working for IMDB.com
+ * @author makinder
+ * @version 0.0.1
+ */
+
+class Evil_Parser_ImdbParser
+{
+	/**
+	 * @desc parse IMDB.com
+	 * @author makinder
+	 * @param string $whatWeNeed(new|top|tv);
+	 * @return Array
+	 * @example returned array
+	 
+	 array(26) {
+  				[0] => array(15) 
+  							{
+							    ["Title"] 		=> "Drive"
+							    ["Year"] 		=> "2011"
+							    ["Rated"] 		=> "N/A"
+							    ["Released"] 	=> "2011-05-24"
+							    ["Genre"] 		=> "Action, Drama"
+							    ["Director"] 	=> "Nicolas Winding Refn"
+							    ["Writer"] 		=> "Hossein Amini, James Sallis"
+							    ["Actors"] 		=> "Ryan Gosling, Carey Mulligan, Bryan Cranston, Christina Hendricks"
+							    ["Plot"] 		=> "A Hollywood stunt performer who moonlights as a wheelman discovers that a contract has been put on him after a heist gone wrong."
+							    ["Poster"] 		=> "N/A"
+							    ["Runtime"] 	=> "1 hr 35 mins"
+							    ["Rating"] 		=> "N/A"
+							    ["Votes"] 		=> "N/A"
+							    ["ID"] 			=> "tt0780504"
+							    ["Response"] 	=> "True"
+							  }
+				}			    
+	 ....................................................................................................................................
+	 * @version 0.0.1
+	 */
+	public function parse($whatWeNeed)
+	{
+		switch ($whatWeNeed)
+		{
+			case 'new': $url = 'http://www.imdb.com/nowplaying/#topten'; break;	
+			case 'top': $url = 'http://www.imdb.com/chart/';break; 
+			case 'tv' : $url = 'http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&title_type=tv_series';break;		
+		}
+
+ 		$total_info = file_get_contents($url);
+        $dom = new Zend_Dom_Query($total_info);                        
+        $links = $dom->query('a');
+        
+        $data = array();        
+        foreach ($links as $item)           	        	        
+        	$data[] = $item->getAttribute('href');                	      
+        
+        $find = '/title/tt';
+        $result = array();        
+		foreach ($data as $index=>$value)
+		{				
+			if (strpos($value, $find) !== false)
+        	{	
+        		$item = substr($value, strpos($value, '/tt')+1, 9);        													        	        		
+        		$result[] = $item;     		
+        	}
+		}		
+		
+		$request = array();
+		$hoho = array_unique($result);					
+		foreach ($hoho as $index=>$id)
+		{												
+			$requesturl = 'http://www.imdbapi.com/?t=' .$id .'&year=2011' ;
+			$request[] = json_decode(file_get_contents($requesturl),true);										
+		}
+										
+		foreach ($request as $index=>$mas)
+			$request[$index]['Released'] = date("Y-m-d", strtotime($mas['Released'])); 		
+		
+		return $request;				
+	}
+}
---
+++ Parser/LastFmApi.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @desc class which working for lastfm api
+ * @author makinder
+ * @version 0.0.1
+ * @example $apiKey= 'c04029734cac3ccf1dccfdf7e45168a3';
+ */
+
+class Evil_Parser_LastFmApi
+{
+	
+	/**
+	 * @desc 
+	 * @author makinder
+	 * @param string $apiKey
+	 * @return array
+	 * @example return array:
+	 
+	 ["track"] => array(50)
+	 				 {
+      					[0] => array(9) 
+      							{
+        							["name"] 	   => "Rolling In The Deep"
+        							["duration"]   => "230"
+							        ["playcount"]  => "119251"
+							        ["listeners"]  => "46283"
+        							["mbid"] 	   => array(0) 
+        												{
+        												}
+        							["url"] 	   =>"http://www.last.fm/music/Adele/_/Rolling+In+The+Deep"
+        							["streamable"] => "0"
+        							["artist"] 	   => array(3) 
+        											{
+          												["name"] => "Adele"
+          												["mbid"] => "b0335a95-8a12-4c71-8149-5054ec847d04"
+          												["url"]  =>  "http://www.last.fm/music/Adele"
+        											}
+        							["image"] 	   => array(4) 
+        											{
+												          [0] 	 =>  "http://userserve-ak.last.fm/serve/34s/55125087.png"
+												          [1] 	 =>  "http://userserve-ak.last.fm/serve/64s/55125087.png"
+												          [2] 	 =>  "http://userserve-ak.last.fm/serve/126/55125087.png"
+												          [3] 	 =>  "http://userserve-ak.last.fm/serve/300x300/55125087.png"
+        											}
+        							}				
+	 	...........................................................................
+	 * @version 0.0.1
+	 */
+	public function getTopTracks($apiKey)
+	{
+		
+		$url = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key='.$apiKey;
+		$request = file_get_contents($url);
+		$xmlobj  = @simplexml_load_string($request);
+		$json = json_encode($xmlobj);
+		$response = json_decode($json,TRUE);
+		return $response;
+	}
+	
+	
+	/**
+	 * @desc 
+	 * @author makinder
+	 * @param string $apikey
+	 * @return Array
+	 * @example return array
+	 
+	 ["track"] => array(50) 
+	 					{
+      						[0] => array(7) 
+      									{
+        									["name"]       => "Drop the World"
+									        ["duration"]   =>  "230"
+									        ["loves"]      =>  "1941"
+									        ["mbid"]       => array(0) 
+									        					{
+        														}
+        									["url"]        => "http://www.last.fm/music/+noredirect/Lil%27+Wayne/_/Drop+the+World"
+        									["streamable"] =>  "0"
+        									["artist"]     => array(3) 
+        													{
+          														["name"] =>  "Lil' Wayne"
+          														["mbid"] =>  "ac9a487a-d9d2-4f27-bb23-0f4686488345"
+          														["url"]  =>  "http://www.last.fm/music/+noredirect/Lil%27+Wayne"
+        													}
+      									}
+	 .................................................................................................
+	 * @version 0.0.1
+	 */
+	public function getLoveTracks($apiKey)
+	{
+		$url = 'http://ws.audioscrobbler.com/2.0/?method=chart.getlovedtracks&api_key='. $apiKey; 
+		$request = file_get_contents($url);
+		$xmlobj  = @simplexml_load_string($request);
+		$json = json_encode($xmlobj);
+		$response = json_decode($json,TRUE);
+		return $response;
+	}
+}
