1be17a301b66a20d562bf72386dc1172f9da270f
Sergey Bukharov <bukharovSI@gmail.com>
2011-09-16 12:56:23 +0400

    Media Parser работает при вызове больше одного парсера

--- Parser/VideoImdb.php
+++ Parser/VideoImdb.php
@@ -130,8 +130,6 @@ class Evil_Parser_VideoImdb implements   Evil_Parser_Interface
 
             $result[$category] = $request;
 		}
-Zend_Debug::dump($result);
-        die();
 		return $result;
     }
 
